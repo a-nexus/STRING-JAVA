e3112d0f744288ab7e09a200f87a633b31830260
a-nexus <anuragsingh77077@gmail.com>
2026-03-27 22:05:17 +0530

    APPEND FUNCTION

--- StringBuilders.java
+++ StringBuilders.java
@@ -25,25 +25,37 @@
 
 //NEXT IS TO INSERT A ALPHABET OR OTHER THINGS IN STRING AT ANY INDEXED VALUE
 
+// import java.util.*;
+// public class StringBuilders{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("ENTER THE NAME:");
+//         StringBuilder sb=new StringBuilder(sc.nextLine());
+//         System.out.print("ENTER THE INDEXED VALUE WHERE YOU WANT TO ADD SOMETING:");
+//         int num=sc.nextInt();
+//         sc.nextLine();
+
+//         System.out.print("ENTER THE THING WHICH YOU WANT TO ADD AT "+num+" INDEXED VALUE:");
+//         String alpha=sc.nextLine();
+//         sb.insert(num,alpha);
+//         System.out.println("Name="+sb);
+//         //IF I ANT TO DELETE THE INSETED VALUE SO HERE IS A FUNCTION NAME DELETE
+//         System.out.print("ENTER THE INDEXED VALUE WHICH YOU WANT TO DELETE:");
+//         int num2=sc.nextInt();
+
+//         sb.delete(num2,num2+1);
+//         System.out.println(sb);
+//     }
+// }
+
+//NEXT APPEND FUNCTION IN WHICH WE CAN ADD THING IN LAST OF THE STRINGBUILDER
+
 import java.util.*;
 public class StringBuilders{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("ENTER THE NAME:");
-        StringBuilder sb=new StringBuilder(sc.nextLine());
-        System.out.print("ENTER THE INDEXED VALUE WHERE YOU WANT TO ADD SOMETING:");
-        int num=sc.nextInt();
-        sc.nextLine();
-
-        System.out.print("ENTER THE THING WHICH YOU WANT TO ADD AT "+num+" INDEXED VALUE:");
-        String alpha=sc.nextLine();
-        sb.insert(num,alpha);
-        System.out.println("Name="+sb);
-        //IF I ANT TO DELETE THE INSETED VALUE SO HERE IS A FUNCTION NAME DELETE
-        System.out.print("ENTER THE INDEXED VALUE WHICH YOU WANT TO DELETE:");
-        int num2=sc.nextInt();
-
-        sb.delete(num2,num2+1);
-        System.out.println(sb);
+           Scanner sc=new Scanner(System.in);
+           StringBuilder sb=new StringBuilder(sc.nextLine());
+           sb.append(sc.nextLine());
+        System.out.print(sb);          
     }
 }
