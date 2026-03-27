7dd44f402185535815778c5ec836187db9301b1d
a-nexus <anuragsingh77077@gmail.com>
2026-03-27 21:25:21 +0530

    INSERT A CHARECTOR TO A STRING

--- StringBuilders.java
+++ StringBuilders.java
@@ -10,14 +10,28 @@
 
 //HERE IS ANOTHER FUNCTION SET CHAR AT FUNCTION IN WHICH WE CAN CHANGE THE INDEX VALUE WHICH WE WANT
 
+// import java.util.*;
+// public class StringBuilders{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         StringBuilder sb=new StringBuilder(sc.nextLine());
+//        sb.setCharAt(0,'s');//FIRSTLY WE CHANGE THE STRING THEN WE GO FOR THE PRINT
+//        System.out.println(sb);
+        
+
+//     }
+// }
+
+
+//NEXT IS TO INSERT A ALPHABET OR OTHER THINGS IN STRING AT ANY INDEXED VALUE
+
 import java.util.*;
 public class StringBuilders{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER THE NAME");
         StringBuilder sb=new StringBuilder(sc.nextLine());
-       sb.setCharAt(0,'s');//FIRSTLY WE CHANGE THE STRING THEN WE GO FOR THE PRINT
-       System.out.println(sb);
-        
-
+        sb.insert(2,'a');
+        System.out.println("Name="+sb);
     }
 }
