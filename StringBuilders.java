8cc11f0d4bb3772529d8ba6ef8c3d20085958676
a-nexus <anuragsingh77077@gmail.com>
2026-03-27 20:32:31 +0530

    STRINGBUILDER AND FUNCTION

---
+++ StringBuilders.java
@@ -0,0 +1,9 @@
+public class StringBuilders {
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("tony");
+        System.out.println("NAME="+sb);
+        System.out.println(sb.charAt(0));
+
+    }
+    
+}
